e705acc0704661a7737255fa4a9a436c4a4227cd
ana117 <63540951+ana117@users.noreply.github.com>
2024-06-03 22:28:02 +0700

    feat: implement web socket on new transaction

--- components/transaction/transaction.tsx
+++ components/transaction/transaction.tsx
@@ -1,21 +1,61 @@
 "use client";
 
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import TransactionList from "./list/transaction-list";
 import TransactionDetail from "./detail/transaction-detail";
 import React from "react";
 import { Transaction } from "@/models/model";
+import { useToast } from "@/components/ui/use-toast";
 
 interface TransactionProps {
   transactions: Transaction[];
 }
 
+const useWebSocket = (url: string) => {
+  const [latestTransaction, setLatestTransaction] = useState<Transaction>();
+
+  useEffect(() => {
+      const socket = new WebSocket(url);
+
+      socket.onopen = () => console.log('WebSocket connected');
+      socket.onmessage = (event) => setLatestTransaction(JSON.parse(event.data));
+      socket.onclose = () => console.log('WebSocket disconnected');
+      return () => socket.close();
+  }, [url]);
+
+  return latestTransaction;
+};
+
+
 const Transactions: FC<TransactionProps> = ({transactions}) => {
   const [curTransaction, setCurTransaction] = useState<Transaction[] | null>(transactions);
   const [activeTransaction, setActiveTransaction] = useState<Transaction | null>(null);
 
+  const { toast } = useToast();
+  const ip = process.env.NEXT_PUBLIC_CONTAINER_BACKEND;
+  const wsUrl = ip ? ip.replace(/^http/, 'ws').replace(/^https/, 'wss') : 'ws://localhost:8000';
+  const latestTransaction = useWebSocket(`${wsUrl}/api/payment/ws/`);
+
+  useEffect(() => {
+    if (latestTransaction) {
+      toast({
+        title: "New Transaction",
+        description: `ID: ${latestTransaction.id}`,
+        duration: 5000,
+      });
+      setCurTransaction((prevTransactions) => {
+        if (prevTransactions) {
+          return [latestTransaction, ...prevTransactions];
+        }
+        return prevTransactions;
+      });
+    }
+  }, [latestTransaction]);
+
   const rowClickedHandler = async (id: string) => {
-    setActiveTransaction(transactions.filter((to) => to.id === id)?.[0]);
+    if (curTransaction) {
+      setActiveTransaction(curTransaction.filter((to) => to.id === id)?.[0]);
+    }
   };
 
   const handleTransactionUpdate = (updatedTransaction: Transaction) => {
